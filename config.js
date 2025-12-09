8f81846e0422fe92f1625b64b21811d1843634de
Bernardo Fusco <bernardol.fusco@gmail.com>
2025-12-09 17:40:20 -0300

    Add config.js for GitHub Pages deployment

---
+++ config.js
@@ -0,0 +1,7 @@
+// Arquivo de configuracao - NAO COMMITAR NO GIT
+// Este arquivo contem seu token privado da API
+
+const CONFIG = {
+    API_BASE_URL: 'https://apiloterias.com.br/app/v2/resultado',
+    API_TOKEN: 'A3V0bpHOgLVuA8k'
+};
